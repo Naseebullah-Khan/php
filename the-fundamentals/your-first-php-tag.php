b72ed7e32884efe94d04766639130de576f61f1a
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-04 20:23:06 +0430

    The Fundamentals - Your First PHP Tag

---
+++ the-fundamentals/your-first-php-tag.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Your First PHP Tag</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo "Hello, World!";
+        ?>
+    </h1>
+    <h2>
+        <?php
+        echo "Hello, universe!";
+        ?>
+    </h2>
+</body>
+
+</html>

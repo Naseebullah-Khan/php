e93801600d9c0735cbd1779e76613c93d96053f3
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-04 20:25:05 +0430

    The Fundamentals - Variables

---
+++ the-fundamentals/variables.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Variables</title>
+</head>
+
+<body>
+  <h1>
+    <?php
+
+    echo "Hello" . " " . "Multiverse!";
+
+    $greetings = "Hello";
+
+    echo "<br/>";
+
+    echo $greetings . " " . "Multiverse!";
+
+    echo "<br/>";
+
+    echo "$greetings Multiverse!";
+
+    echo "<br/>";
+
+    echo '$greetings Multiverse!';
+    ?>
+  </h1>
+</body>
+
+</html>

817eb54b8765d23a3624220c06d8eb53cd90a194
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-16 20:00:42 +0430

    Sessions and Authentication - PHP Sessions 101

---
+++ 05-sessions-and-authentication/01-php-sessions-101/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "database" => [
+        "host" => "localhost",
+        "port" => 3306,
+        "user" => "root",
+        "password" => "root1234",
+        "dbname" => "notes-mini-project",
+        "charset" => "utf8mb4",
+    ]
+];
---
+++ 05-sessions-and-authentication/01-php-sessions-101/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+$router->get(uri: "/", controller: "controllers/index.php");
+$router->get(uri: "/about", controller: "controllers/about.php");
+$router->get(uri: "/contact", controller: "controllers/contact.php");
+
+$router->get(uri: "/notes", controller: "controllers/notes/index.php");
+$router->get(uri: "/note", controller: "controllers/notes/show.php");
+$router->delete(uri: "/note", controller: "controllers/notes/destroy.php");
+
+$router->get(uri: "/notes/create", controller: "controllers/notes/create.php");
+$router->post(uri: "/notes", controller: "controllers/notes/store.php");
+
+$router->get(uri: "/note/edit", controller: "controllers/notes/edit.php");
+$router->patch(uri: "/note", controller: "controllers/notes/update.php");
